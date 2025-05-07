76bbda5129afd24558f5a4bbf370e94f3963eced
butenko_or <o.butenko@arenadc.io>
2025-05-07 23:32:04 +0300

    Added gradle configuration for checkstyle. Small fix in Spring and SpringBoot presentations.

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    id("application")
+    id("checkstyle")
+//    id("pmd")
 }
 
 group = "or.butenko"
@@ -11,14 +13,35 @@ repositories {
 
 dependencies {
     implementation("org.springframework:spring-context:6.2.5")
-
-    //
+    testImplementation("org.springframework:spring-test:6.2.5")
     compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    // Log
+    implementation("ch.qos.logback:logback-classic:1.5.17")
 
     // Test
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.12.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.1")
 }
+
+application {
+    mainClass = "or.butenko.Main"
+}
+
+checkstyle {
+    toolVersion = "10.23.0"
+    configFile = file("config/checkstyle/checkstyle-personal.xml")
+    isIgnoreFailures = true
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+//pmd {
+//    toolVersion = "7.10.0"
+//    isConsoleOutput = true
+//}
 
 tasks.test {
     useJUnitPlatform()
